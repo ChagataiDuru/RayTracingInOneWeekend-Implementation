97d2ec7012839b6c27ef813ee440333671eba74f
Çağatay <durucagatay01@gmail.com>
2024-08-21 16:44:18 +0300

    viewport and hittable

--- main.cpp
+++ main.cpp
@@ -1,26 +1,63 @@
 #include "color.h"
-#include "vec3.h"
+#include "ray.h"
+#include "viewport.h"
 
 #include <iostream>
 
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    else {
+        return (-b - std::sqrt(discriminant)) / (2.0 * a);
+    }
+}
+
+color ray_color(const ray& r) {
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.rayPos(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
 int main() {
+
+    // Render
+
+    auto main_viewport = new viewport(800, 1.78, 1.0);
 
     // Image
 
-    int image_width = 256;
-    int image_height = 256;
+    int image_width = main_viewport->get_image_width();
+    int image_height = main_viewport->get_image_height();
 
-    // Render
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+            auto pixel_center = main_viewport->get_pixel00_loc() + (i * main_viewport->get_pixel_delta_u()) + (j * main_viewport->get_pixel_delta_v());
+            auto camera_center = main_viewport->get_camera_center();
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
 
     std::clog << "\rDone.                 \n";
 }
+
---
+++ viewport.h
@@ -0,0 +1,48 @@
+#ifndef VIEWPORT_H
+#define VIEWPORT_H
+
+#include "vec3.h"
+
+class viewport {
+public:
+    viewport(int image_width, double aspect_ratio, double focal_length)
+        : image_width(image_width),
+        image_height(static_cast<int>(image_width / aspect_ratio)),
+        viewport_height(2.0),
+        viewport_width(viewport_height* aspect_ratio),
+        camera_center(0, 0, 0),
+        viewport_u(viewport_width, 0, 0),
+        viewport_v(0, -viewport_height, 0),
+        pixel_delta_u(viewport_u / image_width),
+        pixel_delta_v(viewport_v / image_height),
+        viewport_upper_left(camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2),
+        pixel00_loc(viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v)) {
+        // Ensure the image height is at least 1
+        image_height = (image_height < 1) ? 1 : image_height;
+    }
+
+    int get_image_width() const { return image_width; }
+    int get_image_height() const { return image_height; }
+
+    const point3& get_camera_center() const { return camera_center; }
+    const vec3& get_viewport_u() const { return viewport_u; }
+    const vec3& get_viewport_v() const { return viewport_v; }
+    const vec3& get_pixel_delta_u() const { return pixel_delta_u; }
+    const vec3& get_pixel_delta_v() const { return pixel_delta_v; }
+    const point3& get_pixel00_loc() const { return pixel00_loc; }
+
+private:
+    int image_width;
+    int image_height;
+    double viewport_height;
+    double viewport_width;
+    point3 camera_center;
+    vec3 viewport_u;
+    vec3 viewport_v;
+    vec3 pixel_delta_u;
+    vec3 pixel_delta_v;
+    point3 viewport_upper_left;
+    point3 pixel00_loc;
+};
+
+#endif // VIEWPORT_H
